6f896f3d4f83505d2070397ea1e880749fdf0410
TPODAvia <thepowerofdarknes2000@gmail.com>
2023-07-26 10:02:06 +0300

    Code updates

--- STM32/blink/blink.c
+++ STM32/blink/blink.c
@@ -2,12 +2,12 @@
 #include <libopencm3/stm32/gpio.h>
 
 int main(void) {
-rcc_periph_clock_enable(RCC_GPIOC);
-gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+rcc_periph_clock_enable(RCC_GPIOB);
+gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO12);
 while(1) {
-    gpio_set(GPIOC, GPIO13);
+    gpio_set(GPIOB, GPIO12);
     for (int i = 0; i < 1000000; ++i) __asm__("nop");
-    gpio_clear(GPIOC, GPIO13);
+    gpio_clear(GPIOB, GPIO12);
     for (int i = 0; i < 500000; ++i) __asm__("nop");
 }
 }
--- STM32/robot_code/robot_code.c
+++ STM32/robot_code/robot_code.c
@@ -1,87 +1,108 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/usart.h>
-#include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/timer.h>
+void init_gpio(void);
+void setup_pwm(void);
 
-int main(void) {
-    // Enable clock for GPIOA and TIM2
-    rcc_periph_clock_enable(RCC_GPIOA);
-    rcc_periph_clock_enable(RCC_TIM2);
+void init_gpio(void)
+{
+    int i;
 
-    // Set PA6 to alternate function (AF) mode for TIM2_CH3
-    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_TIM2_CH3);
+    /* Enable GPIOB clock. */
+    rcc_periph_clock_enable(RCC_GPIOB);
 
-// PWM setup
-// Reset TIM2
-// timer_reset(TIM2);
-
-// Set the Timer mode to no divisor (72MHz), Edge-aligned, up-counting
-timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
-
-// Set the prescaler to 72
-timer_set_prescaler(TIM2, 72);
-
-// Disable preload
-timer_disable_preload(TIM2);
-
-// Continuous mode
-timer_continuous_mode(TIM2);
-
-// Set the auto-reload value (ARR)
-timer_set_period(TIM2, 1000);
-
-// Set the PWM mode for channel 3 - PWM mode 1
-timer_set_oc_mode(TIM2, TIM_OC3, TIM_OCM_PWM1);
-
-// Set the output compare value for OC1
-timer_set_oc_value(TIM2, TIM_OC3, 500);
-
-// Enable the output compare - OC1
-timer_enable_oc_output(TIM2, TIM_OC3);
-
-// Enable the Timer
-timer_enable_counter(TIM2);
-
-    // Enable clock for USART1 and GPIOA
-    rcc_periph_clock_enable(RCC_USART1);
-    rcc_periph_clock_enable(RCC_GPIOA);
-
-    // Set PA9 to alternate function (USART1_TX) and PA10 to input (USART1_RX)
-    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX);
-    gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO_USART1_RX);
-
-// USART setup
-usart_set_baudrate(USART1, 9600);
-usart_set_databits(USART1, 8);
-usart_set_stopbits(USART1, USART_STOPBITS_1);
-usart_set_mode(USART1, USART_MODE_TX_RX);
-usart_set_parity(USART1, USART_PARITY_NONE);
-usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
-
-usart_enable(USART1);
-
-    // Enable USART1 interrupt
-    nvic_enable_irq(NVIC_USART1_IRQ);
-    nvic_set_priority(NVIC_USART1_IRQ, 0);
-
-while (1) {
-    // Check if there is data available to read
-    if (usart_get_flag(USART1, USART_FLAG_RXNE)) {
-        char received_data = usart_recv(USART1);
-
-        // Here you can process the received data
-        // For example, let's print it back to the USART
-        usart_send_blocking(USART1, received_data);
+    /* Set GPIO0 (in GPIO port B) to 'output push-pull'. */
+    for(i = 0; i < 16; i++)
+    {
+        if(i != 12)
+        {
+            gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
+                          GPIO_CNF_OUTPUT_PUSHPULL, (1 << i));
+            gpio_clear(GPIOB, (1 << i));
+        }
     }
+
+    /* Set GPIO12 (in GPIO port B) to 'output alternate function push-pull'. */
+    gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_2_MHZ,
+                  GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO12);
 }
+
+void setup_pwm(void)
+{
+    /* Reset TIM4 peripheral. */
+    rcc_periph_reset_pulse(RST_TIM4);
+
+    /* Clock division and mode. */
+    timer_set_mode(TIM4, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+
+    /* Reset prescaler value. */
+    timer_set_prescaler(TIM4, 0);
+
+    /* Enable preload. */
+    timer_disable_preload(TIM4);
+
+    /* Continous mode. */
+    timer_continuous_mode(TIM4);
+
+    /* Period (36MHz/1000). */
+    timer_set_period(TIM4, 1000);
+
+    /* Disable outputs. */
+    timer_disable_oc_output(TIM4, TIM_OC1);
+    timer_disable_oc_output(TIM4, TIM_OC2);
+    timer_disable_oc_output(TIM4, TIM_OC3);
+    timer_disable_oc_output(TIM4, TIM_OC4);
+
+    /* -- OC1 configuration -- */
+
+    /* Configure global mode of line 1. */
+    timer_disable_oc_clear(TIM4, TIM_OC1);
+    timer_disable_oc_preload(TIM4, TIM_OC1);
+    timer_set_oc_slow_mode(TIM4, TIM_OC1);
+    timer_set_oc_mode(TIM4, TIM_OC1, TIM_OCM_PWM1);
+
+    /* Set the capture compare value for OC1. */
+    timer_set_oc_value(TIM4, TIM_OC1, 100);
+
+    /* Enable OC1. */
+    timer_enable_oc_output(TIM4, TIM_OC1);
+
+    /* ---- */
+
+    /* ARR reload enable. */
+    timer_disable_preload(TIM4);
+
+    /* Counter enable. */
+    timer_enable_counter(TIM4);
+
+    /* Enable commutation interrupt. */
+    timer_enable_irq(TIM4, TIM_DIER_CC1IE);
+
+    /* Enable TIM4. */
+    timer_enable_counter(TIM4);
+}
+
+int main(void)
+{
+    /* Initialize GPIOs. */
+    init_gpio();
+
+    /* Setup PWM. */
+    setup_pwm();
+
+    /* Main loop. */
+    while (1)
+    {
+        int i;
+        // for(i = 0; i <= 100; i++)
+        // {
+        //     timer_set_oc_value(TIM4, TIM_OC1, i);
+        //     for(int j = 0; j < 100000; j++) /* Delay */
+        //     {
+        //         __asm__("nop");
+        //     }
+        // }        
+    }
 
     return 0;
-}
-
-void usart1_isr(void) {
-    if (usart_get_flag(USART1, USART_FLAG_RXNE)) {
-        char received_data = usart_recv(USART1);
-        // Process received_data
-    }
 }
